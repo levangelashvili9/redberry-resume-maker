58fd83b32e32bf481d112b68cf56e0016ddd9894
Levan Gelashvili <levan.gelashvili9@gmail.com>
2023-02-12 00:33:33 +0400

    updated utils

--- src/common/utils/isRequired.ts
+++ src/common/utils/isRequired.ts
@@ -1,7 +1,7 @@
 export const isRequired = (obj: any, index: number) => {
   if (index !== 0 && Object.values(obj).every((value) => !value)) {
     return false;
-  } else if (index !== 0 && Object.values(obj).some((value) => value !== "")) {
+  } else if (index !== 0 && Object.values(obj).some((value) => value)) {
     return true;
   } else {
     return true;
--- src/common/utils/removeEmptyObjects.ts
+++ src/common/utils/removeEmptyObjects.ts
@@ -1,5 +1,5 @@
 export function removeEmptyObjects(array: object[]): object[] {
   return array.filter((obj) => {
-    return Object.values(obj).some((val) => val !== "" && val !== null);
+    return Object.values(obj).some((val) => val);
   });
 }
